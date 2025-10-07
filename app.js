010cb5dae798f930c5651e56c3a644143ca48af1
Shalina Rohdenburg <shalinarohdenburg@gmail.com>
2025-10-07 13:46:43 +0200

    feat: add dynamic route for activities

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { logger } from "./middlewares/logger.js";
+import { activities } from "./data/activities.js";
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,20 @@ const PORT = 3000;
 app.use(logger);
 
 app.use("/", express.static("public"));
+
+app.get("/activities/:id", (request, response) => {
+  const activityId = request.params.id;
+  const activity = activities.find((activity) => activity.id === activityId);
+
+  if (!activity)
+    return response.send(
+      "Activity not found. Please check your spelling and try again."
+    );
+
+  return response.send(
+    `${activity.name} at ${activity.venue}. ${activity.description}. Price: ${activity.price} per person.`
+  );
+});
 
 app.listen(PORT, () => {
   console.log("The server has restarted");
