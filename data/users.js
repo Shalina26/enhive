955b8bcb046a82a8daf00d7302fa86e28c487056
Shalina Rohdenburg <shalinarohdenburg@gmail.com>
2025-10-07 13:43:12 +0200

    feat: add user mock data

---
+++ data/users.js
@@ -0,0 +1,12 @@
+export const users = [
+  { username: "pixelTiger", password: "RiverStone123" },
+  { username: "nebulaFox", password: "SunnyMeadow" },
+  { username: "lunarQuest", password: "CoffeeRocket" },
+  { username: "echoFalcon", password: "LemonTwist" },
+  { username: "cyberOtter", password: "MapleForest" },
+  { username: "solarViper", password: "PepperMinty" },
+  { username: "mysticRaven", password: "SilverAcorn" },
+  { username: "quantumMoth", password: "VelvetCloud" },
+  { username: "velvetWolf", password: "OceanBreeze" },
+  { username: "orbitLynx", password: "TigerLily22" },
+];
