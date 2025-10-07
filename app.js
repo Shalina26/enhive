6ebc1edd0180292d8c12aa6d1478571915123df6
Shalina Rohdenburg <shalinarohdenburg@gmail.com>
2025-10-07 13:47:44 +0200

    feat: add login route

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { logger } from "./middlewares/logger.js";
 import { activities } from "./data/activities.js";
+import { users } from "./data/users.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", express.static("public"));
 
@@ -21,6 +23,21 @@ app.get("/activities/:id", (request, response) => {
   return response.send(
     `${activity.name} at ${activity.venue}. ${activity.description}. Price: ${activity.price} per person.`
   );
+});
+
+app.post("/login", (request, response) => {
+  const logindata = request.body;
+  const username = logindata.username;
+  const password = logindata.password;
+
+  if (!username || !password)
+    return response.send("Please fill out all required fields.");
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) return response.send("Username or password wrong.");
+
+  return response.redirect("/activities.html");
 });
 
 app.listen(PORT, () => {
