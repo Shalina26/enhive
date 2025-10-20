1c56c152bb0f5d6d10015b58da7b832f0f4aae2c
Shalina Rohdenburg <shalinarohdenburg@gmail.com>
2025-10-20 22:18:55 +0200

    refactor: use routers and add database connection

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from "express";
+import mongoose from "mongoose";
 import { logger } from "./middlewares/logger.js";
-import { activities } from "./data/activities.js";
-import { users } from "./data/users.js";
+import { authRouter } from "./routes/auth.js";
+import { activitiesRouter } from "./routes/activities.js";
 
 const app = express();
 const PORT = 3000;
@@ -18,45 +19,20 @@ app.get("/", (request, response) => {
   response.render("index");
 });
 
-app.get("/activities", (request, response) => {
-  response.render("activities/index", { activities });
-});
-
 app.get("/about", (request, response) => {
   response.render("about");
 });
 
-app.get("/login", (request, response) => {
-  response.render("auth/login");
-});
+app.use(authRouter);
+app.use(activitiesRouter);
 
-app.get("/activities/:id", (request, response) => {
-  const activityId = request.params.id;
-  const activity = activities.find((activity) => activity.id === activityId);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/enhive")
+  .then(() => {
+    console.log("💽 Database connected");
 
-  if (!activity)
-    return response
-      .status(404)
-      .send("Activity not found. Please check your spelling and try again.");
-
-  return response.status(200).render("activities/show", { activity: activity });
-});
-
-app.post("/login", (request, response) => {
-  const logindata = request.body;
-  const username = logindata.username;
-  const password = logindata.password;
-
-  if (!username || !password)
-    return response.send("Please fill out all required fields.");
-
-  const user = users.find((user) => user.username === username);
-
-  if (!user) return response.send("Username or password wrong.");
-
-  return response.redirect("activities");
-});
-
-app.listen(PORT, () => {
-  console.log("The server has restarted");
-});
+    app.listen(PORT, () => {
+      console.log("The server has restarted");
+    });
+  })
+  .catch((error) => console.error(error));
