3c3bc132d999d33498bb77816ec77d545d99af58
Shalina Rohdenburg <shalinarohdenburg@gmail.com>
2025-10-28 14:20:30 +0100

    refactor: use seperate config files

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from "express";
-import mongoose from "mongoose";
 import { logger } from "./middlewares/logger.js";
 import { authRouter } from "./routes/auth.js";
 import { activitiesRouter } from "./routes/activities.js";
 import { simplePagesRouter } from "./routes/simple-pages.js";
+import { connectDb } from "./config/database.js";
+import { PORT } from "./config/app.js";
 
 const app = express();
-const PORT = 3000;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -20,13 +20,16 @@ app.use(simplePagesRouter);
 app.use("/activities", activitiesRouter);
 app.use(authRouter);
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/enhive")
-  .then(() => {
-    console.log("💽 Database connected");
+const onSuccess = () => {
+  console.log("💽 Database connected");
 
-    app.listen(PORT, () => {
-      console.log("The server has restarted");
-    });
-  })
-  .catch((error) => console.error(error));
+  app.listen(PORT, () => {
+    console.log("The server has restarted");
+  });
+};
+
+const onError = (error) => {
+  console.error(error);
+};
+
+connectDb().then(onSuccess).catch(onError);
