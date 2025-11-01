b0174fe124ae0fe5f3a5fd55039f88e66688551c
Shalina Rohdenburg <shalinarohdenburg@gmail.com>
2025-11-01 19:40:32 +0100

    refactor: remove old validation and update slug creator

--- helpers/activity-views.js
+++ helpers/activity-views.js
@@ -7,21 +7,8 @@ export const createPriceInCents = (priceInEuro) => {
 };
 
 export const createSlug = (name, venue) => {
-  const trimmedName = name.trim();
-  const trimmedVenue = venue.trim();
-  const combinedNameAndVenue = trimmedName + " " + trimmedVenue;
-  const slug = combinedNameAndVenue.replace(/ /g, "-");
+  const combinedNameAndVenue = (name + " " + venue).trim();
+  const slug = combinedNameAndVenue.replace(/[^A-Za-z0-9]+/g, "-");
 
   return slug.toLowerCase();
-};
-
-export const validateNameAndVenue = (name, venue) => {
-  const regex = /^[A-Za-z0-9\- ]+$/;
-  const validName = regex.test(name);
-  const validVenue = regex.test(venue);
-
-  if (validName && validVenue) {
-    return true;
-  }
-  return false;
 };
