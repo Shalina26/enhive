3833e10b791bb57a1bf90b23a2976c0390f7416f
Shalina Rohdenburg <shalinarohdenburg@gmail.com>
2025-11-01 19:38:28 +0100

    reafctor: add validation middleware to activity routes

--- routes/activities.js
+++ routes/activities.js
@@ -8,13 +8,38 @@ import {
   renderEditActivityPage,
   deleteActivity,
 } from "../controllers/activities.js";
+import { validateInput } from "../middlewares/validation.js";
+import {
+  descriptionValidationChain,
+  nameValidationChain,
+  priceValidationChain,
+  venueValidationChain,
+} from "../validations/index.js";
 
 export const activitiesRouter = express.Router();
 
 activitiesRouter.get("/", renderAllActivitiesPage);
 activitiesRouter.get("/new", renderAddNewActivityPage);
-activitiesRouter.post("/new", createNewActivity);
+activitiesRouter.post(
+  "/new",
+  validateInput([
+    nameValidationChain,
+    venueValidationChain,
+    descriptionValidationChain,
+    priceValidationChain,
+  ]),
+  createNewActivity
+);
 activitiesRouter.get("/:slug", renderActivityPage);
-activitiesRouter.post("/:slug", updateActivity);
+activitiesRouter.post(
+  "/:slug",
+  validateInput([
+    nameValidationChain,
+    venueValidationChain,
+    descriptionValidationChain,
+    priceValidationChain,
+  ]),
+  updateActivity
+);
 activitiesRouter.get("/:slug/edit", renderEditActivityPage);
 activitiesRouter.get("/:slug/delete", deleteActivity);
