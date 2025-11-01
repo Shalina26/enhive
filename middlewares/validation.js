7412c3b866040dcd43649742b354dcc13309ae76
Shalina Rohdenburg <shalinarohdenburg@gmail.com>
2025-11-01 19:35:12 +0100

    feat: create middleware to validate input

---
+++ middlewares/validation.js
@@ -0,0 +1,9 @@
+export const validateInput = (validationChains) => {
+  return async (response, request, next) => {
+    for (const chain of validationChains) {
+      await chain.run(response);
+    }
+
+    next();
+  };
+};
