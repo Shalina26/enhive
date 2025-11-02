e16ce85bd68e6416986ec0237bc25310f2ebc651
Shalina Rohdenburg <shalinarohdenburg@gmail.com>
2025-11-02 16:11:35 +0100

    refactor: change database name

--- config/database.js
+++ config/database.js
@@ -2,5 +2,7 @@ import mongoose from "mongoose";
 import { MONGODB_URI } from "./app.js";
 
 export const connectDb = () => {
-  return mongoose.connect(MONGODB_URI);
+  return mongoose.connect(MONGODB_URI, {
+    dbName: "enhive",
+  });
 };
